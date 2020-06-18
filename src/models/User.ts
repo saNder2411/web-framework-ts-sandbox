8e5070d9cf4d76751985131c4ac5d0f9233d45ef
Alexander Olshanskiy <sander2411ukr@gmail.com>
2020-06-18 19:43:30 +0300

    refactoring attrs  property whith composition

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,3 +1,4 @@
+import { Attributes } from './Attributes';
 import { Eventing } from './Eventing';
 import { Sync } from './Sync';
 
@@ -11,17 +12,12 @@ interface UserProps {
 const rootUrl = `http://localhost:3000/users`;
 
 export class User {
+  public attributes: Attributes<UserProps>;
   public events: Eventing = new Eventing();
   public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
 
-  constructor(private data: UserProps ) {}
-
-  get(propName: string): string | number {
-    return this.data[propName];
-  }
-
-  set(update: UserProps): void {
-    Object.assign(this.data, update);
+  constructor(attrs: UserProps) {
+    this.attributes = new Attributes<UserProps>(attrs);
   }
    
 }
