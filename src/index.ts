94d0070965761a22570eb58a62a3106b0dab2924
Alexander Olshanskiy <sander2411ukr@gmail.com>
2020-06-18 11:08:20 +0300

    create user model ans add get, set methods

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log('hi there!');
+import { User } from './models/User';
+
+const user = new User({ name: 'Sasha', age: 32});
+
+user.set({name: 'vera'});
+console.log(user.get('name'));
+console.log(user.get('age'));
