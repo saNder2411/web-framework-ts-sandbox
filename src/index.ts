734142d684fde5a3151f01d9197b6ef9705efd1c
Alexander Olshanskiy <sander2411ukr@gmail.com>
2020-06-18 11:49:57 +0300

    add events handle for user model

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,10 @@ import { User } from './models/User';
 
 const user = new User({ name: 'Sasha', age: 32});
 
-user.set({name: 'vera'});
-console.log(user.get('name'));
-console.log(user.get('age'));
+user.on('click', () => console.log('click 1'));
+user.on('change', () => console.log('change 1'));
+user.on('click', () => console.log('click 2'));
+user.trigger('click');
+user.trigger('change');
+
+console.log(user);
--- src/models/User.ts
+++ src/models/User.ts
@@ -3,7 +3,11 @@ interface UserProps {
   age?: number;
 }
 
+type Callback = () => void;
+
 export class User {
+
+  events: { [key: string]: Callback[] } = {};
 
   constructor(private data: UserProps ) {}
 
@@ -13,6 +17,19 @@ export class User {
 
   set(update: UserProps): void {
     Object.assign(this.data, update);
+  }
+
+  on(eventName: string, callback: Callback): void {
+    const handlers = this.events[eventName] || [];
+    this.events[eventName] = [...handlers, callback];
+  }
+
+  trigger(eventName: string): void {
+    const handlers = this.events[eventName];
+ 
+    if (!handlers || handlers.length === 0) return;
+    
+    handlers.forEach((callback) => callback());
   }
    
 }
