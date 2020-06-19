62e9d5c6c02053c4ba694297d452a4085bec3673
Alexander Olshanskiy <sander2411ukr@gmail.com>
2020-06-19 11:43:31 +0300

    create view user form

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
-import { User } from './models/User';
+import { UserForm } from './views/UserForm';
 
 
-const collection = User.createUserCollection();
+const userForm = new UserForm(document.getElementById('root'));
 
-collection.on('change', () => console.log(collection));
-
-collection.fetch();
+userForm.render();
 
