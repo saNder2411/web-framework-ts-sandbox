199e0297fadd3ad9e5ffb0a9275a0520ffe4e208
Alexander Olshanskiy <sander2411ukr@gmail.com>
2020-06-19 19:24:35 +0300

    user form complet

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -7,6 +7,7 @@ export class UserForm extends View<User, UserProps> {
     return {
       'click:.set-age': this.onSetAgeClick,
       'click:.set-name': this.onSetNameClick,
+      'click:.save-model': this.onSaveClick,
     }
   }
 
@@ -23,15 +24,17 @@ export class UserForm extends View<User, UserProps> {
     this.model.set({ name });
   };
 
+  onSaveClick = (): void => {
+    this.model.save();
+  }
+
   template(): string {
     return `
       <div>
-        <h1>User Form</h1>
-        <div>User name: ${this.model.get('name')}</div>
-        <div>User age: ${this.model.get('age')}</div>
-        <input type="text" name="userName" />
+        <input type="text" name="userName" placeholder="${this.model.get('name')}" />
         <button class="set-name">Change Name</button>
         <button class="set-age">Set Random Age</button>
+        <button class="save-model">Save User</button>
       </div>
     `;
   }
